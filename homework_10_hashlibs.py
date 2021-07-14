2a87765b34cc68f5824cfb94de9932908b29ff7f
JUDY MAKAKA <79365316+makakajudy@users.noreply.github.com>
2021-07-14 21:32:40 +0100

    Update homework_10_hashlibs.py

--- homework_10_hashlibs.py
+++ homework_10_hashlibs.py
@@ -37,44 +37,70 @@ def password_authenticating(db_password, user_password):
     return hashed_password == db_password
 
 
-user_name = "red"  # input("enter user name: ")
-user_passwd = "black"  # input("user enter a password: ")
-# saved_password = hashing(user_passwd)
+def admin_only():  # for higher level admin use.used to reset password incase a user has forgotten
+    read_file = open("admin.txt", 'r')
+    for i in read_file:
+        print(i, end='')
+    read_file.close()
 
-# write_file = open("mypw.txt", 'a')
-# write_file.write("\n" + user_name + "," + user_passwd)
-# write_file.close()
-my_dict = {}
-read_file = open("mypw.txt", 'r')
-for line in read_file.readlines():
-    user, pw = line.split(",")
-    pw = pw.strip()
-    my_dict.update({user: pw})
 
-print(my_dict)
-i = 0
-# while i < 6:
-#    if (i == "3"):
-#        print("y")
-#        break
-#    i += 1
-# else:print("n")
-access = False
-while i < len(my_dict) and access != True:
-    for x, y in my_dict.items():
-        if x == user_name and y == user_passwd:
-            print("access granted")
-            access = True
+def creating_pw(user_name, user_passwd):  # for a higher level admin use.to add new admin passwords
+    write_file = open("judy.txt", 'a')
+    write_file.write("\n" + user_name + "," + hashing(user_passwd))
+    write_file.close()
 
-            break
-    # print("j")
 
-    i += 1
-if access == False:
-    print("access denied")
+def use_login(user_name, user_passwd):
+    my_dict = {}
+    read_file = open("judy.txt", 'r')
+    for line in read_file.readlines():
+        user, pw = line.split(",")
+        pw = pw.strip()
+        my_dict.update({user: pw})
+    # key_check(user_name, my_dict)# to check entered user name is in the dict
+    # print(my_dict)
+    i = 0
+    saved_password = my_dict[user_name]
+    access = False
+    while i < len(my_dict) and access != True:
+        for x, y in my_dict.items():
+            if x == user_name and saved_password:
+                if password_authenticating(saved_password, user_passwd):
+                    print("\u001b[32m access granted\u001b[0m")
+                    access = True
+                    break
+        i += 1
+    if access == False:
+        print("\u001b[31maccess denied\u001b[0m")
+    read_file.close()
 
-read_file.close()
 
-# print(password_authenticating(saved_password, "lise"))
-#print(password_authenticating(saved_password,userpassword))
+def key_check(user_name, my_dict):
+    if user_name not in my_dict:
+        return "invalid user name"
 
+
+def instruction():
+    return """\u001b[35mThis program demonstrates the use of hashlib using bkdf2_hmac and other SHA.via hashing() and password_authenticating().
+    How it works:
+    There is already a file with saved logging credentials in the judy.txt with passwords already hashed.
+    There is also another file with unhashed passwords so that it human readable only to be used
+    when unsure of your password.
+    This program does not cater for unknown username yet.\u001b[0m
+    
+     
+     \u001b[32m***** Please use SAVED USERNAMES ONLY to test**** \u001b[0m"""
+
+
+admin_only() #uncomment and only use first time to see saved password
+hint: print(chr(27) + "[2J") 
+print()
+print(instruction())
+print()
+print("ADMIN LOGINS")
+print()
+print("\u001b[32mplease enter the below credentials to login\u001b[0m")
+
+username = input("enter user name: ")
+password = input("user enter a password: ")
+use_login(username, password)
